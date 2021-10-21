17b93f233fbfb55b4b373d6359e99a211c26890f
Shreesh <tanaykumar103@gmail.com>
2021-10-21 22:44:58 +0530

    revamped code base

--- update.py
+++ update.py
@@ -1,133 +1,192 @@
-#!/usr/bin/python
+# version = 0.1
+# date: 21/10/2021
 
-#Author: Shreesh Kumar Verma
-#Mail: tanaykumar103@gmail.com
-#Date: 22/09/2021
-
-
-import os
+import argparse, os, pickle
 import datetime as dt
-import pickle, argparse
-
-class Helper:
-	dateToday = dt.date.today()
-
-	# location of pickle file name 'data' to load
-	# change this variable as per requirement
-	#data = os.environ['HOME'] + '/.local/arch-updatehelper/data'
-	data = os.path.expanduser('~/.local/arch-updatehelper/data')
 
 
-	def __init__(self):
-		
-		# Option feature to change difference between days of updates
-		parser = argparse.ArgumentParser(description='Options to modify difference between dates')
+class AUH:
 
-		# --D optional argument added to change difference
-		parser.add_argument('--D', required= False, type=int, help='Value sets difference between dates')
-		parser.add_argument('-v', required=False, action='store_true', help='Show next updatable date')
-		parser.add_argument('--A', required=False, type=str, help='Change AUR helper, Default is yay')
+    '''
+    available methods
 
+    1) Change AUR helper -> changeHelper()
+    2) Change AUR Command -> changeCommand()
+    3) Change Difference between updates -> changeDifferece()
+    4) Retrive information from pickle file -> getInfo()
+    6) Provide information from pickle -> moreInfo()
+    7) Run Updater -> runUpdater()
+    8) Write Data -> writeData()
+    9) Write post update information -> writeDefault()
+    '''
 
-		# add all arguments to parser object
-		# self.args contains all the arguments passed and their values.
-		self.args = parser.parse_args()
+    # multiple use variables
+    PickleLocation = os.path.expanduser('~/projects/arch-updatehelper/test/data')
+    PickleData = {'diff': None,'lastDate': None, 'helper': None,'command': None}
+    dateToday = dt.date.today()
 
+    def __init__(self):
+        
+        
+        parser = argparse.ArgumentParser(description='Options to modify difference between dates')
 
-		# checking if any valid --D argument has been passed
-		# if True self.writeData() is called and value of diff key is changed
-		# else self.main() is called and update is carried on
-		if self.args.D is not None:
-			self.writeData()
-
-		elif self.args.v is True:
-			self.moreInfo()
-
-		elif self.args.A is not None:
-			self.writeData()
-		else:
-			self.main()
-
-
-	# provided some extra info on next 'updateable-date'
-	def moreInfo(self):
-
-		info = self.getData()
-		
-		# lastDate of update + difference value - today's date,
-		# Output-> Next update will be available after dd/mm/YYYY
-		info_ = str((info[1] + dt.timedelta(info[0])))
-
-		print(f'Next update will be available after {info_} \nCurrent helper:{info[2]}')
+        parser.add_argument('-D', required= False, type=int, help='Value sets difference between dates')
+        parser.add_argument('-V', action = 'store_true', required=False, help='Show next updatable date')
+        parser.add_argument('-A', required=False, type=str, help='Change AUR helper, Default is yay')
+        parser.add_argument('-C', required=False, type=str, help='Customize update command, Default= -Syyu')
 
 
-	# this method retrieves data from pickle file named data stored at location self.data
-	# and return's list of [diff,lastDate]
-	def getData(self):
-		with open(self.data, 'rb') as file:
+        # add all arguments to parser object
+        # self.args contains all the arguments passed and their values.
+        self.args = parser.parse_args()
 
-			data_ = pickle.load(file)
+        if self.args.A is not None:
+            self.changeHelper()
 
-			diff = data_['diff']
-			lastDate = data_['lastDate']
-			helperName = data_['helper']
+        elif self.args.C is not None:
+            self.changeCommand()
 
-		return [diff, lastDate, helperName]
+        elif self.args.D is not None:
+            self.changeDifference()
 
-	# this method writes to pickle file stored at location self.data
-	def writeData(self):
-		
-		# checking if valid --D argument is passed
-		# if true, diff key's value is changed to --D's value
-		diff = self.args.D
-		helperName = self.args.A
+        elif self.args.V is True:
+            self.moreInfo()
 
-		# writes changes in 'diff' values with other remaining same
-		if diff is not None and int:
-			data_ = {'diff':diff,'lastDate':self.dateToday, 'helper':self.getData()[2]}	
-			print(f'Difference beteen updates changed to {diff}.')
+        else:
+            self.runUpdater()
 
-		# writes changes in 'helperName' with other remaining same
-		elif helperName is not None and str:
-			data_ = {'diff':self.getData()[0],'lastDate':self.getData()[1], 'helper':helperName}
-			print(f'AUR helper changed from {self.getData()[2]} to {helperName}')
-		
-		# writes default value only, return value from self.getData()
-		else:
-			data_ = {'diff':self.getData()[0],'lastDate':self.dateToday, 'helper':self.getData()[2]}
-		file = open(self.data, 'wb')
+    def changeHelper(self):
 
-		pickle.dump(data_, file)
-		file.close()
+        helper = self.args.A
+        info = self.getInfo()
 
-	# lifeline and reason of this whole script.
-	# this method actually runs the package manager
-	def main(self):
+        self.PickleData['diff'] = info[0]
+        self.PickleData['lastDate'] = info[1]
+        self.PickleData['helper'] = helper
+        self.PickleData['command'] = info[3]
 
-		# retrieve data from pickle file
-		info = self.getData()
+        try:
+            self.writeData()
+            print(f'AUR helper changed to: {helper}')
 
-		# check for difference between date of last update and current date
-		# if current date > lastdate then run package manager, else wait.
-		if info[1] + dt.timedelta(days=info[0]) < dt.date.today():
-			print('alright... get ready to be updated')
-			exit_code = os.system(f'{info[2]} -Syyu')
-			
-			# conditional to check for exit code of last statement
-			# if exit_code == 0, self.writeData() is called else
-			# output is generated
-			if exit_code == 0:
-				self.writeData()
-			else:
-				print("Failed in updating")
-		
-		# checking if -v option provided
-		# if true, moreinfo() is called
-		else:
-		#	if self.args.v is True:
-		#		self.moreInfo()
-			
-			print('Wait some time')
+        except:
+            print('AUR helper not changed error occured')
+
+
+    def changeCommand(self):
+        
+        command = self.args.C
+        info = self.getInfo()
+
+        self.PickleData['diff'] = info[0]
+        self.PickleData['lastDate'] = info[1]
+        self.PickleData['helper'] = info[2]
+        self.PickleData['command'] = '-' + command
+
+        try:
+            self.writeData()
+            print(f'Command changed to: {command}')
+
+        except:
+            print('Command not changed error occured')
+
+
+    def changeDifference(self):
+        
+        diff = self.args.D
+        info = self.getInfo()
+        
+        self.PickleData['diff'] = diff
+        self.PickleData['lastDate'] = info[1]
+        self.PickleData['helper'] = info[2]
+        self.PickleData['command'] = info[3]
+
+        try:
+            self.writeData()
+            print(f'Difference changed to: {diff}')
+
+        except:
+            print('Difference not changed error occured')
+
+
+    def getInfo(self):
+        
+        try:
+            with open(self.PickleLocation, 'rb') as PickleFile:
+                data = pickle.load(PickleFile)
+
+                diff = data['diff']
+                lastDate = data['lastDate']
+                helper = data['helper']
+                command = data['command']
+
+            return [diff, lastDate, helper, command]
+        
+        except:
+            print ('Error in retrieving info')
+
+    
+    def moreInfo(self):
+        
+        info = self.getInfo()
+
+        output = f'''Difference: {info[0]}.\nLast updated on: {info[1]}.\nCurrent helper: {info[2]}.\nCommand used: {info[3]}'''
+
+        print(output)
+
+
+    def runUpdater(self):
+        
+        # retrieve data from pickle file
+        info = self.getInfo()
+
+        # check for difference between date of last update and current date
+        # if current date > lastdate then run package manager, else wait.
+        if info[1] + dt.timedelta(days=info[0]) < self.dateToday:
+            print('alright... get ready to be updated')
+            exit_code = os.system(f'{info[2]} + {info[3]}')
+            
+            # conditional to check for exit code of last statement
+            # if exit_code == 0, self.writeData() is called else
+            # output is generated
+            if exit_code == 0:
+                self.writeDefault()
+            else:
+                print("Failed in updating")
+
+        # checking if -v option provided
+        # if true, moreinfo() is called
+        else:
+        #	if self.args.v is True:
+        #		self.moreInfo()
+            
+            print('Wait some time')
+
+    
+    def writeData(self):
+        try:
+            with open(self.PickleLocation, 'wb') as PickleFile:
+                pickle.dump(self.PickleData, PickleFile)
+
+        except:
+            print('Error in writing data')
+
+
+    def writeDefault(self):
+        helper = self.args.A
+        info =self.getInfo()
+
+        self.PickleData['diff'] = info[0]
+        self.PickleData['lastDate'] = self.dateToday
+        self.PickleData['helper'] = info[2]
+        self.PickleData['command'] = info[3]
+
+        try:
+            self.writeData()
+            print('Update information saved')
+
+        except:
+            print('Update information not saved error occured')
 
 if __name__ == '__main__':
-	obj = Helper()
+    AUH()
